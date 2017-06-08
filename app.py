fc6658aae433f8ac1cdf7f1b1fa86068f8a8ef09
pmaslankowski <pmaslankowski@gmail.com>
2017-06-09 01:23:45 +0200

    App file cleaned

--- app.py
+++ app.py
@@ -2,50 +2,28 @@
 # Author: Piotr Maślankowski
 # Databases, UWr, June 2017
 
-import sys, json
+import sys
 from database import Database
 from query import Query
-
-class QueryBuilder:
-    def build_query(self, json_object):
-        command = json.loads(json_object)
-        cmd_name = list(command.keys())[0]
-
-        try:
-            build_query = getattr(self, cmd_name)
-        except AttributeError:
-            raise NotImplementedError('Function {} is not implemented'.format(cmd_name))
-
-        arguments = command[cmd_name]
-        query_data = build_query(cmd_name, arguments)
-        return Query(*query_data)
-
-
-    def event(self, name, args):
-        columns = []
-        query = 'INSERT INTO event(name, start_date, finish_date) VALUES(%s, %s, %s)'
-        placeholders = [args['eventname'], args['start_timestamp'], args['end_timestamp']]
-        auth_data = (args['login'], args['password'], 'organiser')
-        return name, columns, query, placeholders, True, auth_data
-
-
+from query_builder import QueryBuilder
+from unit_tests import *
 
 builder = QueryBuilder()
-q = builder.build_query('{"event":{"login": "test2", "password": "password2", "eventname": "name1", "start_timestamp":"22-10-2015", "end_timestamp": "23-10-2015"}}')
-print(q)
+q1 = builder.build_query(test_event)
+q2 = builder.build_query(test_user)
+q3 = builder.build_query(test_talk)
+q4 = builder.build_query(test_register_user_for_event)
+q5 = builder.build_query(test_attendance)
+q6 = builder.build_query(test_evaluation)
+q7 = builder.build_query(test_reject)
+q8 = builder.build_query(test_reject2)
+q9 = builder.build_query(test_proposal)
+q10 = builder.build_query(test_talk2)
+q11 = builder.build_query(test_friends)
 
 db = Database(True)
 db.open('conference_database', 'app_api', '123456')
-db.execute(q)
-q.parse_result()
-print(q.result)
+db.execute(q11)
+q11.parse_result()
+print(q11.result)
 db.close()
-
-#query1 = Query(['id','name', 'start_date', 'finish_date'],
-#               'SELECT id, name, start_date, finish_date FROM event',
-#               None)
-
-#db.execute(query1)
-#query1.parse_result()
-# print(query1.result)
-# db.close()
