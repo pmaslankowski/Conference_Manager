61492bc1a0f3ff6d03219064e9da4d5c92952483
pmaslankowski <pmaslankowski@gmail.com>
2017-06-09 01:23:06 +0200

    Unit tests for finished queries

---
+++ unit_tests.py
@@ -0,0 +1,17 @@
+# Conference Manager App
+# Author: Piotr Maślankowski
+# Databases, UWr, June 2017
+
+
+
+test_event = '{"event":{"login": "test2", "password": "password2", "eventname": "Event2", "start_timestamp":"22-10-2015", "end_timestamp": "23-10-2015"}}'
+test_user = '{"user": {"login": "test3", "password": "password3", "newlogin": "testuser", "newpassword": "testuser"}}'
+test_talk = '{"talk": {"login": "test3", "password": "password3", "speakerlogin": "test1", "talk": "talk4", "title": "Talk4Title", "start_timestamp": "15-05-2017", "room": 120, "initial_evaluation": 5, "eventname": "Event1"}}'
+test_register_user_for_event = '{"register_user_for_event": {"login": "test2", "password": "password2", "eventname": "Event1"}}'
+test_attendance = '{"attendance": {"login": "test1", "password": "password1", "talk": "talk1"}}'
+test_evaluation = '{"evaluation": {"login": "test3", "password": "password3", "talk": "talk4", "rating": 10}}'
+test_reject = '{"reject": {"login": "test3", "password": "password3", "talk": "talk1"}}'
+test_reject2 = '{"reject": {"login": "test3", "password": "password3", "talk": "talk4"}}'
+test_proposal = '{"proposal": {"login": "test2", "password": "password2", "talk": "talk_proposition", "title": "wild title", "start_timestamp": "16-05-2017 15:00", "eventname": "Event1"}}'
+test_talk2 = '{"talk": {"login": "test3", "password": "password3", "speakerlogin": "test1", "talk": "talk_proposition", "title": "Wild Title 2", "start_timestamp": "20-05-2017", "room": 130, "initial_evaluation": 4, "eventname": "Event1"}}'
+test_friends = '{"friends": {"login1": "test1", "password": "password1", "login2": "test3"}}'
