54d5862290d77f81e9d443fb985491ce1fd10b89
pmaslankowski <pmaslankowski@gmail.com>
2017-06-09 01:21:46 +0200

    Something changed

--- tests.sql
+++ tests.sql
@@ -1,19 +1,22 @@
 -- Test trigger on invitation_friend_of
-INSERT INTO conf_user(login, password) VALUES('test1', 'password1');
-INSERT INTO conf_user(login, password) VALUES('test2', 'password2');
-INSERT INTO conf_user(login, password, usertype) VALUES ('test3', 'password3', 'organiser');
+INSERT INTO conf_user(id, password) VALUES('test1', 'password1');
+INSERT INTO conf_user(id, password) VALUES('test2', 'password2');
+INSERT INTO conf_user(id, password, usertype) VALUES ('test3', 'password3', 'organiser');
 
 -- case 1:
-INSERT INTO invitation_friend_of VALUES(1, 2);
-INSERT INTO invitation_friend_of VALUES(2, 1);
+INSERT INTO invitation_friend_of VALUES('test1', 'test2');
+INSERT INTO invitation_friend_of VALUES('test2', 'test1');
 
 -- case 2:
-INSERT INTO invitation_friend_of VALUES(2, 1);
+INSERT INTO invitation_friend_of VALUES('test2', 'test1');
 
 -- case 3:
-INSERT INTO event(name, start_date, finish_date)
+INSERT INTO event(id, start_date, finish_date)
 VALUES ('Event1', '10-05-2017', '20-05-2017');
-INSERT INTO talk(userid, eventid, title, start_timestamp)
-VALUES(1, 1, 'talk1', '15-05-2017 8:00');
-INSERT INTO talk(userid, eventid, title, start_timestamp)
-VALUES(2, 1, 'talk2', '22-05-2017 8:00');
+INSERT INTO talk(id, userid, eventid, title, start_timestamp)
+VALUES('talk1', 'test1', 'Event1', 'TalkTitle1', '15-05-2017 8:00');
+INSERT INTO talk(id, userid, eventid, title, start_timestamp)
+VALUES('talk2', 'test2', 'Event1', 'TalkTitle2', '22-05-2017 8:00');
+
+-- case 4:
+SELECT create_user('nowy_user', '123456');
